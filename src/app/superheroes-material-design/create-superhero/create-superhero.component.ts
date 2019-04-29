6eb4e475ac3c5e66d09e15d5a4820e31a0d93277
V Keerti Kotaru <VenCKi-Work@Venkatas-MacBook-Pro.local>
2019-04-29 08:03:43 -0400

    Create superhero component created

---
+++ src/app/superheroes-material-design/create-superhero/create-superhero.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-create-superhero',
+  templateUrl: './create-superhero.component.html',
+  styleUrls: ['./create-superhero.component.css']
+})
+export class CreateSuperheroComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/superheroes-material-design/superheroes-material-design.module.ts
+++ src/app/superheroes-material-design/superheroes-material-design.module.ts
@@ -1,30 +1,39 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field'
+import { MatInputModule } from '@angular/material/input';
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatCardModule} from '@angular/material/card';
 import { SuperheroProfileComponent } from './superhero-profile/superhero-profile.component';
 import { SuperheroToolbarComponent } from './superhero-toolbar/superhero-toolbar.component';
 import HitCounter from './utilities/hit-counter';
 import { SuperheroProfileFooterComponent } from './superhero-profile-footer/superhero-profile-footer.component';
 import { SuperheroListComponent } from './superhero-list/superhero-list.component';
+import { CreateSuperheroComponent } from './create-superhero/create-superhero.component';
 
 @NgModule({
   declarations: [
     SuperheroProfileComponent, 
     SuperheroToolbarComponent, 
-    SuperheroProfileFooterComponent, SuperheroListComponent, 
+    SuperheroProfileFooterComponent, 
+    SuperheroListComponent, 
+    CreateSuperheroComponent, 
   ],
   imports: [
     BrowserAnimationsModule, 
+    
+    MatInputModule,
     MatCardModule, 
-    MatToolbarModule
+    MatToolbarModule,
+    MatFormFieldModule
   ],
   exports: [
     SuperheroProfileComponent,
     SuperheroToolbarComponent,
-    SuperheroProfileFooterComponent
+    SuperheroProfileFooterComponent,
+    CreateSuperheroComponent
   ],
   providers:[HitCounter]
 })
