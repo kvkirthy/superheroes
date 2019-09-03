ee4b6f4b99565d8d8bbd58cacef29c91773560a6
V Keerti Kotaru <VenCKi-Work@Venkatas-MBP.hsd1.ma.comcast.net>
2019-09-03 01:29:31 -0400

    Icon added to snippet

--- src/app/code-samples/code-samples.module.ts
+++ src/app/code-samples/code-samples.module.ts
@@ -3,13 +3,15 @@ import { CommonModule } from '@angular/common';
 import { Chapter4Component } from './chapter4/chapter4.component';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   declarations: [Chapter4Component],
   imports: [
     CommonModule,
     MatSidenavModule,
-    MatCardModule
+    MatCardModule,
+    MatIconModule
   ]
 })
 export class CodeSamplesModule { }
