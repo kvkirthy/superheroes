a081ea4850e73da1a939ef7e2b5afee451a47cff
V Keerti Kotaru <kvkirthy@gmail.com>
2019-09-05 09:34:30 -0400

    Chapter5 added

---
+++ src/app/code-samples/chapter5/chapter5.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-chapter5',
+  templateUrl: './chapter5.component.html',
+  styleUrls: ['./chapter5.component.css']
+})
+export class Chapter5Component implements OnInit {
+
+  spanTwoCells = "2";
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/code-samples/code-samples.module.ts
+++ src/app/code-samples/code-samples.module.ts
@@ -4,9 +4,10 @@ import { Chapter4Component } from './chapter4/chapter4.component';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
+import { Chapter5Component } from './chapter5/chapter5.component';
 
 @NgModule({
-  declarations: [Chapter4Component],
+  declarations: [Chapter4Component, Chapter5Component],
   imports: [
     CommonModule,
     MatSidenavModule,
